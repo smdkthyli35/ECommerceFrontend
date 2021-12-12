5ad2ad3a47e98ca3f295326db35934e23986a373
zeljkosamed <smdkthyli@gmail.com>
2021-12-13 00:01:14 +0300

    Ürünler listelendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  products:any[];
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit():void{
+    this.http.get('https://localhost:44310/api/products/getproductdetails').subscribe((response:any)=>{
+      this.products = response.data;
+    },error=>{
+      console.log(error)
+    });
+  }
 }
